f20d053691814bbdb33d8582d5ceaa873184ddac
Killian Monnier <paraceltus@gmail.com>
2020-04-29 22:52:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ var imgtitre;
 
 
 function preload(){
-	imgfond = loadImage ("../img/home/cheminForet.png");
-	imgfondinv = loadImage ("../img/home/cheminForetInverse.png");
-	imgtitre = loadImage ("../img/home/titre.png");
+	imgfond = loadImage ("img/home/cheminForet.png");
+	imgfondinv = loadImage ("img/home/cheminForetInverse.png");
+	imgtitre = loadImage ("img/home/titre.png");
 }
 
 function setup() {
